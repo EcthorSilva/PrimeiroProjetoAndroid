442b2fd707ffa673f42fa9f76b3b68c5f8148868
Ecthor <EcthorSilva@outlook.com>
2021-03-08 20:22:42 -0300

    Novas activity adicionadas

---
+++ app/src/main/java/br/com/local/primeiroprojeto/Cadastro_Usuario_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.primeiroprojeto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cadastro_Usuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_usuario_layout);
+    }
+}
