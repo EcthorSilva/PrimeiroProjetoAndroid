b5d980131fc91ec1bdafbd07aae126757d6d5bf3
Ecthor <EcthorSilva@outlook.com>
2021-04-07 20:31:21 -0300

    Update 07/04

--- app/src/main/java/br/com/local/primeiroprojeto/Login_Activity.java
+++ app/src/main/java/br/com/local/primeiroprojeto/Login_Activity.java
@@ -5,20 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
+    EditText edtUsuario, edtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtSenha = findViewById(R.id.edtSenha);
     }
 
     public void loginToPrincipal(View view) {
+
+        String usuario, senha;
+
+        usuario = edtUsuario.getText().toString();
+        senha = edtSenha.getText().toString();
+
+        if (usuario.equals("ecthor") && senha.equals("1234")) {
+            Intent irPrincipal = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(irPrincipal);
+        } else {
+            Toast.makeText(getApplicationContext(), "Usuario ou senha invalidos",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void singUpToPrincipal(View view) {
         // mostra uma mensagem para o usuario
         //Toast.makeText(getApplicationContext(), "Carregando", Toast.LENGTH_LONG ).show();
 
-        //abrir janela do main activity
+        //abrir janela de cadastro
 
         Intent abrirJanela = new Intent(getApplicationContext(), Cadastro_Usuario_Activity.class);
         startActivity(abrirJanela);
